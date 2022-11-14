335d505f8a641c7feec18d3c490c2226ae98946f
Cyryl <folkbread@gmail.com>
2022-11-14 15:15:40 +0100

    for max-width: 768

--- project_10/src/js/script.js
+++ project_10/src/js/script.js
@@ -1,11 +1,25 @@
 function hamburger() {
 
     const btnBurger = document.querySelector(".menu_burger"),
-          menu = document.querySelector(".menu");
+          menu = document.querySelector(".menu"),
+          menuLinks = document.querySelectorAll(".menu_item > a");
+
+    hideMenuAfterClick(menu, btnBurger);
 
     function checkActiveClass(menuBrg, crossBtn) {
         crossBtn.classList.toggle('menu_burger_active');
-        return menuBrg.classList.toggle('active');
+        return menuBrg.classList.toggle('active_mobile_menu');
+    }
+    function hideMenuAfterClick(menuBrg, crossBtn) {
+        menuLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                if(crossBtn.classList.contains('menu_burger_active') && menuBrg.classList.contains('active_mobile_menu')) {
+                    crossBtn.classList.remove('menu_burger_active');
+                    menuBrg.classList.remove('active_mobile_menu');
+                }
+                return; 
+            })
+        })
     }
 
     btnBurger.addEventListener("click", () => {
