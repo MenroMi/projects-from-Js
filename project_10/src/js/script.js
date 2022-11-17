eef1f82547b38c780baf1aa756585ca90d09b21a
Cyryl <folkbread@gmail.com>
2022-11-17 19:27:14 +0100

    ...

--- project_10/src/js/script.js
+++ project_10/src/js/script.js
@@ -1,31 +1,51 @@
-function hamburger() {
+window.addEventListener("DOMContentLoaded", () => {
 
-    const btnBurger = document.querySelector(".menu_burger"),
-          menu = document.querySelector(".menu"),
-          menuLinks = document.querySelectorAll(".menu_item > a");
-          
-    hideMenuAfterClick(menu, btnBurger);
+    function scrollOnRequirements() {
 
-    function checkActiveClass(menuBrg, crossBtn) {
-        crossBtn.classList.toggle('menu_burger_active');
-        return menuBrg.classList.toggle('active_mobile_menu');
+        const btnRequire = document.querySelector("#require"),
+              requirementsCoords = document.querySelector("#grapefruit");
+
+        btnRequire.addEventListener('click', () => {
+            return document.documentElement.scrollTo(0, requirementsCoords.offsetTop);
+        });
+
+
     }
-    function hideMenuAfterClick(menuBrg, crossBtn) {
-        menuLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                if(crossBtn.classList.contains('menu_burger_active') && menuBrg.classList.contains('active_mobile_menu')) {
-                    crossBtn.classList.remove('menu_burger_active');
-                    menuBrg.classList.remove('active_mobile_menu');
-                }
-                return; 
+
+    scrollOnRequirements();
+
+
+    function hamburger() {
+
+        const btnBurger = document.querySelector(".menu_burger"),
+              menu = document.querySelector(".menu"),
+              menuLinks = document.querySelectorAll(".menu_item > a");
+              
+        hideMenuAfterClick(menu, btnBurger);
+    
+        function checkActiveClass(menuBrg, crossBtn) {
+            crossBtn.classList.toggle('menu_burger_active');
+            return menuBrg.classList.toggle('active_mobile_menu');
+        }
+        function hideMenuAfterClick(menuBrg, crossBtn) {
+            menuLinks.forEach(link => {
+                link.addEventListener('click', () => {
+                    if(crossBtn.classList.contains('menu_burger_active') && menuBrg.classList.contains('active_mobile_menu')) {
+                        crossBtn.classList.remove('menu_burger_active');
+                        menuBrg.classList.remove('active_mobile_menu');
+                    }
+                    return; 
+                })
             })
-        })
+        }
+    
+        btnBurger.addEventListener("click", () => {
+            checkActiveClass(menu, btnBurger);
+        });
+    
     }
+    
+    hamburger();
 
-    btnBurger.addEventListener("click", () => {
-        checkActiveClass(menu, btnBurger);
-    });
 
-}
-
-hamburger();
+});
