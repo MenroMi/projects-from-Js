ea175cfdbcb4e15e21aa54a82e51763b15a0b29f
Cyryl <folkbread@gmail.com>
2022-11-18 22:35:40 +0100

    make first step in validation and update data in inputs with imgs card

--- project_07/src/js/script.js
+++ project_07/src/js/script.js
@@ -4,10 +4,22 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const formForPost = document.querySelector("form"),
           wrapperRequest = document.querySelector(".wrapper_after_request"),
-          btnContinue = document.querySelector(".request_button");
+          btnSubmit = document.querySelector(".data_btn"),
+          btnContinue = document.querySelector(".request_button"),
+          inputs = document.querySelectorAll("input"),
+          cardholderName = document.querySelector(".card_front_name"),
+          cardholderNumber = document.querySelector(".card_front_number"),
+          cardholderDate = document.querySelector(".card_front_date"),
+          errorCardName = document.querySelector(".data_form_error");
 
     postData(formForPost);
-    afterRequestForm(wrapperRequest, formForPost    );
+    afterPost(wrapperRequest, formForPost);
+    
+    inputs.forEach((input) => {
+        input.addEventListener("input", () => {
+            validationData(inputs, errorCardName);
+        })
+    })
 
 
     function postData(form) {
@@ -27,6 +39,14 @@ window.addEventListener("DOMContentLoaded", () => {
 
             request.send(formData);
 
+            request.addEventListener("error", (e) => {
+                e.preventDefault();
+                form.append(divError);
+                setTimeout(() => {
+                    divError.remove();
+                }, 3000);
+            })
+
             request.addEventListener("load", () => {
                 if(request.status === 200) {
                     console.log(request.response);
@@ -45,7 +65,7 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    function afterRequestForm(requestForm, form) {
+    function afterPost(requestForm, form) {
         btnContinue.addEventListener("click", () => {
             if(form.classList.contains("hidden")) {
                 requestForm.classList.add('hidden');
@@ -56,5 +76,39 @@ window.addEventListener("DOMContentLoaded", () => {
         return;
     }
 
+    function validationData(inputs, error) {
+
+        inputs.forEach(input => {
+
+            switch(input.getAttribute("id")) {
+                case "name":
+                    validateCardName(input, error);
+                    break;
+            }
+
+
+        })
+
+        function validateCardName(input, error) {
+
+            if(input.value.match(/^[\w\W]{0,10}$/i) && input.value.match(/\D/g) && !input.value.match(/^[>#*!(){}[\]]+$/g)) {
+                error.classList.add("hidden");
+                btnSubmit.disabled = false;
+                input.style.border = "1px solid black";
+                cardholderName.textContent = input.value;
+            } else if (input.value.length === 0) {
+                error.classList.add("hidden");
+                cardholderName.textContent = "name surname";
+                return input.style.border = "1px solid black";
+            } else {
+                error.classList.remove("hidden");
+                btnSubmit.disabled = true;
+                return input.style.border = "4px solid hsl(0, 100%, 66%)";
+            }
+
+            return;
+
+        }
+    }
 
 });
