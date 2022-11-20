d136683ddf8449dbeaa2c161b9b278d120ac826c
Cyryl <folkbread@gmail.com>
2022-11-20 19:56:26 +0100

    make validation and display on pics

--- project_07/src/js/script.js
+++ project_07/src/js/script.js
@@ -7,7 +7,10 @@ window.addEventListener("DOMContentLoaded", () => {
         btnSubmit = document.querySelector(".data_btn"),
         btnContinue = document.querySelector(".request_button"),
         cardName = document.querySelector(".card_front_name"),
-        cardNumber = document.querySelector(".card_front_number");
+        cardNumber = document.querySelector(".card_front_number"),
+        cardMonth = document.querySelector(".card_front_date_m"),
+        cardYear = document.querySelector(".card_front_date_y"),
+        cardCVV = document.querySelector(".card_back_cvv");
 
     const usernameEl = document.querySelector("#username"),
         numberEl = document.querySelector("#cardnumber"),
@@ -17,31 +20,41 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const isRequired = value => value === '' ? true : false;
     const checkLength = (length, min, max) => length < min || length > max ? true : false;
-    const checkSpecialCharactersAndNumbers = value => {
-        const regex = /(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,./?])|(?=(.*[0-9]))/;
-        return regex.test(value);
-    };
-    const isCardNumber = value => {
-        const regex = /^((4\d{3})|(5[1-5]\d{2}))(-?|\040?)(\d{4}(-?|\040?)){3}|^(3[4,7]\d{2})(-?|\040?)\d{6}(-?|\040?)\d{5}/;
-        return console.log(regex.test(value));
+    const checkSpecialCharactersAndNumbers = value => (/(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,./?])|(?=(.*[0-9]))/).test(value);
+    const isCardNumber = value => (/(?=(.*[a-zA-Z]))|(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,./?])/).test(value);
+    const isMonth = value => (/^((0?[1-9])|((1)[0-2]))$/).test(value);
+    const isYear = value => (/^((0?[1-9])|(([0-9])[0-9])|99)$/).test(value);
 
-    };
-    const isMonth = value => value.test(/^((0?[1-9])|((1|2)[0-9])|12)$/) ? true : false;
-    const isYear = value => value.test(/^((0?[1-9])|((1|2)[0-9])|99)$/) ? true : false;
     const showError = (input, msg) => {
         input.classList.add("form-error");
+        let divField;
 
-        const divField = input.parentElement;
+        if(input.id === "month" || input.id === "year") {
+            console.log(input.id);
+            divField = input.parentElement.parentElement;
+        } else {
+            divField = input.parentElement;
+        }
         const errorMsg = divField.querySelector("small");
         errorMsg.style.color = 'red';
+        btnSubmit.disabled = 'true';
 
         return errorMsg.textContent = msg;
     };
+
     const showSuccess = input => {
         input.classList.remove("form-error");
+        let divField;
 
-        const divField = input.parentElement;
+        if(input.id === "month" || input.id === "year") {
+            divField = input.parentElement.parentElement;
+        } else {
+            divField = input.parentElement;
+        }
+
         const errorMsg = divField.querySelector("small");
+        btnSubmit.disabled = 'false';
+
         return errorMsg.textContent = '';
     }
 
@@ -109,80 +122,142 @@ window.addEventListener("DOMContentLoaded", () => {
     function validateData(form) {
         form.addEventListener('input', function (e) {
 
-            let parts = []
+            let parts = [];
+            const value = e.target.value,
+                  id = e.target.id;
 
-            switch (e.target.id) {
+            switch (id) {
                 case 'username':
-                    if (e.target.value.length > 25) {
+                    if (value.length > 25) {
                         validateUsername(usernameEl);
                         return;
                     }
-                    cardName.textContent = e.target.value;
+                    cardName.textContent = value;
                     validateUsername(usernameEl);
                     break;
                 case 'cardnumber':
-                    if (e.target.value.length > 16) {
-                        validateCardNumber(numberEl);
+
+                    for (let i=0, len=value.length; i<len; i+=4) { // spacebar on front card picture
+                        parts.push(value.substring(i, i+4))
+                      }
+
+                    if (value.length > 16) {
+                        validateCardNumber(numberEl); // without call function here didnt worked validation with amount of characters
+                        return; // without return all numbers are displayed on pic front card 
                     }
-                    for (let i=0, len=e.target.value.length; i<len; i+=4) {
-                        parts.push(e.target.value.substring(i, i+4))
-                      }
-                      if (parts.length) {
-                        cardNumber.textContent = parts.join(' ');
-                      } else {
-                          return e.target.value;
-                      }
+                    
+                    if (parts.length) {
+                    cardNumber.textContent = parts.join(' ');
+                    }
+
                     validateCardNumber(numberEl);
                     break;
-                case 'password':
-                    checkPassword();
+                case 'month':
+                    cardMonth.textContent = value.length === 1 ? `0${value}` : value;
+                    validateCardMonth(monthEl);
                     break;
-                case 'confirm-password':
-                    checkConfirmPassword();
+                case 'year':
+                    cardYear.textContent = value.length === 1 ? `0${value}` : value;
+                    validateCardYear(yearEl);
                     break;
+                case "code-cvv":
+                    if (value.length > 3) {
+                        validateCardCVV(cvvCodeEl);
+                        return;
+                    }
+                    cardCVV.textContent = value;
+                    validateCardCVV(cvvCodeEl);
             }
         });
     }
 
     function validateUsername(username) {
 
-        let valid = false;
-        let min = 3,
-            max = 25;
+        let min = 3, max = 25;
         const name = username.value.trim();
 
         if (isRequired(name)) {
             showError(username, "Can't be blank.");
-        } else if (checkLength(name.length, 3, 25)) {
+        } else if (checkLength(name.length, min, max)) {
             showError(username, `Wrong length. Your cardname must be between ${min} and ${max} characters.`);
         } else if (checkSpecialCharactersAndNumbers(name)) {
             showError(username, "Without special characters or numbers.");
         }
         else {
             showSuccess(username);
-            valid = true;
         }
-
-        return valid;
 
     }
 
     function validateCardNumber(numbers) {
-        let valid = false;
+
+        let min = 16, max = 16;
         const numb = numbers.value.trim();
 
         if (isRequired(numb)) {
             showError(numbers, "Can't be blank.");
-        } else if (!isCardNumber(numb)) {
-            showError(numbers, "Card number must have 16 numbers.");
+        } else if (isCardNumber(numb)) {
+            showError(numbers, "Wrong format. Only numbers");
+        } else if (checkLength(numb.length, min, max)) {
+            showError(numbers, "Must be 16 characters.");
         } else {
             showSuccess(numbers);
-            valid = true;
         }
 
-        return valid;
-    } 
+    }
 
+    function validateCardMonth(month) {
 
+        let min = 1, max = 2;
+        const numb = month.value.trim();
+
+        if (isRequired(numb)) {
+            showError(month, "Can't be blank.");
+        } else if (isCardNumber(numb)) {
+            showError(month, "Only numbers.");
+        } else if (!isMonth(numb)) {
+            showError(month, "From 01 to 12.");
+        } else if (checkLength(numb.length, min, max)) {
+            showError(month, "Must be 2 characters.");
+        } else {
+            showSuccess(month);
+        }
+
+    }
+
+    function validateCardYear(year) {
+
+        let min = 1, max = 2;
+        const numb = year.value.trim();
+
+        if (isRequired(numb)) {
+            showError(year, "Can't be blank.");
+        } else if (isCardNumber(numb)) {
+            showError(year, "Only numbers.");
+        } else if (!isYear(numb)) {
+            showError(year, "From 01 to 99.");
+        } else if (checkLength(numb.length, min, max)) {
+            showError(year, "Must be 2 characters.");
+        } else {
+            showSuccess(year);
+        }
+
+    }
+
+    function validateCardCVV(cvv) {
+
+        let min = 3, max = 3;
+        const numb = cvv.value.trim();
+
+        if (isRequired(numb)) {
+            showError(cvv, "Can't be blank.");
+        } else if (isCardNumber(numb)) {
+            showError(cvv, "Only numbers.");
+        } else if (checkLength(numb.length, min, max)) {
+            showError(cvv, "Must be 3 characters.");
+        } else {
+            showSuccess(cvv);
+        }
+    }
 
 });
