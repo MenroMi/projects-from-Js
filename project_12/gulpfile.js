03cd8256bd6cf67cd11d3e5e84c54f569ffd7e2c
Cyryl <folkbread@gmail.com>
2022-11-25 12:02:01 +0100

    new test project -> min gulp build

---
+++ project_12/gulpfile.js
@@ -0,0 +1,48 @@
+import gulp from "gulp";
+import browserSync from "browser-sync";
+
+
+// Plugins
+
+import GulpCleanCss from "gulp-clean-css";
+import rename from "gulp-rename";
+import autoPrefixer from "gulp-autoprefixer";
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+
+
+// Tasks
+
+const server = () => {
+    return browserSync.init({
+        server: {
+            baseDir: './src/'
+        }
+    })
+};
+
+const renderScssCode = () => {
+    return gulp.src(['./src/scss/*.scss', './src/scss/**/*.scss'])
+        .pipe(sass({
+            outputStyle: "compressed"
+        }).on("error", sass.logError))
+        .pipe(rename({suffix: ".min", prefix: ''}))
+        .pipe(autoPrefixer({
+            browsers: ["last 2 versions"],
+            cascade: false,
+        }))
+        .pipe(GulpCleanCss({compatibility: "ie8"}))
+        .pipe(gulp.dest('./src/css/'))
+        .pipe(browserSync.stream());
+};
+
+const watchers = () => {
+    gulp.watch('./src/scss/*.+(sass|scss)');
+    gulp.watch("./src/scss/*.html").on("change", browserSync.reload);
+}
+
+const dev = gulp.parallel(server, styleSass, watchers);
+
+gulp.task('default', dev);
+
