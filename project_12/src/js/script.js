2fc09d454a2512adc59109b258d7fe2d0ca42c31
Cyryl <folkbread@gmail.com>
2022-11-28 19:47:28 +0100

    responsive styles

--- project_12/src/js/script.js
+++ project_12/src/js/script.js
@@ -5,5 +5,52 @@ $('').ready(function () {
         prevArrow: `<button type="button" class="slick-prev"><img src="icons/slider-arrows/left-arrow.png" alt="left-arrow"></button>`,
         nextArrow: `<button type="button" class="slick-next"><img src="icons/slider-arrows/right-arrow.png" alt="right-arrow"></button>`,
         dots: true,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 991.98,
+                settings: {
+                    infinite: false,
+                    speed: 1100,
+                    adaptiveHeight: true,
+                    dots: true,
+                    prevArrow: `<button type="button" class="slick-prev"><img src="icons/slider-arrows/left-arrow.png" alt="left-arrow" style="display: none;"></button>`,
+                    nextArrow: `<button type="button" class="slick-next"><img src="icons/slider-arrows/right-arrow.png" alt="right-arrow" style="display: none;"></button>`
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    infinite: false,
+                    speed: 1100,
+                    adaptiveHeight: true,
+                    dots: true,
+                    prevArrow: `<button type="button" class="slick-prev"><img src="icons/slider-arrows/left-arrow.png" alt="left-arrow" style="display: none;"></button>`,
+                    nextArrow: `<button type="button" class="slick-next"><img src="icons/slider-arrows/right-arrow.png" alt="right-arrow" style="display: none;"></button>`
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    infinite: false,
+                    speed: 1100,
+                    adaptiveHeight: true,
+                    dots: true,
+                    prevArrow: `<button type="button" class="slick-prev"><img src="icons/slider-arrows/left-arrow.png" alt="left-arrow" style="display: none;"></button>`,
+                    nextArrow: `<button type="button" class="slick-next"><img src="icons/slider-arrows/right-arrow.png" alt="right-arrow" style="display: none;"></button>`,
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    infinite: false,
+                    speed: 1100,
+                    adaptiveHeight: true,
+                    dots: true,
+                    prevArrow: `<button type="button" class="slick-prev"><img src="icons/slider-arrows/left-arrow.png" alt="left-arrow" style="display: none;"></button>`,
+                    nextArrow: `<button type="button" class="slick-next"><img src="icons/slider-arrows/right-arrow.png" alt="right-arrow" style="display: none;"></button>`,
+                }
+            }
+        ]
     });
 });
