e5162e744ffc94a89cf6a2f924feeec77846b4d2
Cyryl <folkbread@gmail.com>
2023-02-20 15:56:39 +0100

    some corrects and make costumer simple slider

--- project_15/src/js/script.js
+++ project_15/src/js/script.js
@@ -1,7 +1,13 @@
-function readMoreBtn() {
+readMoreBtn();
+slider();
+
+function readMoreBtn(trans = false) {
   const parents = document.querySelectorAll(".card__features"),
     cards = document.querySelectorAll(".card");
-  parents.forEach((parent, i) =>
+
+  //   let transition = trans;
+
+  parents.forEach((parent, i) => {
     parent.addEventListener("click", (e) => {
       let features;
       const target = e.target;
@@ -14,10 +20,65 @@ function readMoreBtn() {
           }
         }
       }
-    })
+    });
+  });
+}
+
+// Slider
+
+function changeElements(elems, nameClass, arrayOffset) {
+  const [from, to] = arrayOffset;
+  elems.forEach((elem, i) =>
+    i >= from && i <= to
+      ? elem.classList.add(nameClass)
+      : elem.classList.remove(nameClass)
   );
 }
 
-readMoreBtn();
+function slider() {
+  let cardBox = document.querySelector(".cards");
+  const cardList = document.querySelectorAll(".card");
+  const arrowPrev = document.querySelector(".slider-arrow_prev"),
+    arrowNext = document.querySelector(".slider-arrow_next");
+  let elementsOnPage = 3,
+    pages = Math.ceil(cardList.length / elementsOnPage),
+    actualPage = 1,
+    to = elementsOnPage - 1,
+    from = to - elementsOnPage < 0 ? 0 : to - elementsOnPage;
+  const onFlexCards = (parent, cards) => {
+    let count = 0;
+    cards.forEach((card) => {
+      card.classList.contains("card_active") ? count++ : null;
+    });
 
-function slider() {}
+    return count < 3
+      ? (parent.style.justifyContent = "flex-start")
+      : (parent.style.justifyContent = "");
+  };
+
+  changeElements(cardList, "card_active", [from, to]);
+
+  arrowPrev.addEventListener("click", () => {
+    if (actualPage <= 1) return (actualPage = 1);
+    if (from < 3) return (from = 0);
+
+    actualPage--;
+    from -= elementsOnPage;
+    to -= elementsOnPage;
+    changeElements(cardList, "card_active", [from, to]);
+    onFlexCards(cardBox, cardList);
+    readMoreBtn(true);
+  });
+
+  arrowNext.addEventListener("click", () => {
+    if (actualPage >= pages) return pages;
+    if (to >= cardList.length) return (to = cardList.length);
+
+    actualPage++;
+    to += elementsOnPage;
+    from += elementsOnPage;
+    changeElements(cardList, "card_active", [from, to]);
+    onFlexCards(cardBox, cardList);
+    readMoreBtn(true);
+  });
+}
